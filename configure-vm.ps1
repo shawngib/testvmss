6b591f852130a5796bf1905502748df5e523f279
Shawn Gibbs <shawngib@microsoft.com>
2018-04-10 13:15:22 -0400

    Initial commit.

---
+++ configure-vm.ps1
@@ -0,0 +1,16 @@
+Set-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' -RemoteAddress 10.0.48.8
+
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$letters = 70..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "datadisk"
+
+foreach ($disk in $disks) {
+    $driveLetter = $letters[$count].ToString()
+    $disk | 
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $label.$count -Confirm:$false -Force
+$count++
+}
